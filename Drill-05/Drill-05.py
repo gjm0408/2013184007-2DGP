b095aeb7ad2156ea8e87e1b443781526b6d38586
gjm0408 <gjm940408@naver.com>
2018-09-18 18:15:49 +0900

    폴더 설정
    따로놀던 파일들을 합산

--- Drill-05/Drill-05.py
+++ Drill-05/Drill-05.py
@@ -24,10 +24,22 @@ def move_up():
         delay(0.01)
 
 def move_left():
-    pass
+    x, y = 800 - 25, 50 + 40
+    while x < 600 - 50:
+        clear_canvas_now()
+        grass.draw_now(400, 30)
+        character.draw_now(x, y)
+        x -= 2
+        delay(0.01)
 
 def move_down():
-    pass
+    x, y = 800 - 25, 50 + 40
+    while x < 600 - 50:
+        clear_canvas_now()
+        grass.draw_now(400, 30)
+        character.draw_now(x, y)
+        y -= 2
+        delay(0.01)
 
 def move_left_to_center():
     pass
@@ -35,7 +47,7 @@ def move_left_to_center():
 
 def make_rectangle():
     # move_from_center_to_right
-    move_up
+    # move_up
     move_left()
     move_down()
     move_left_to_center
