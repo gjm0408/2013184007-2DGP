27054361fdae107d11294e746c0b1d8fddff29b0
gjm0408 <gjm940408@naver.com>
2018-09-20 18:36:39 +0900

    Drill06-눈장갑
    눈장갑 추가

--- Drill-06/Drill-06.py
+++ Drill-06/Drill-06.py
@@ -5,39 +5,63 @@ KPU_WIDTH, KPU_HEIGHT = 1280, 1024
 
 def handle_events():
     global running
-    global x , y
+    global x,y
+    global dirx
+    global diry
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
             running = False
         elif event.type == SDL_MOUSEMOTION:
-            x ,y = event.x,KPU_HEIGHT -1 - event.y
+            x,y = event.x, KPU_HEIGHT - 1 - event.y
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             running = False
+        #elif event.type == SDL_MOUSEBUTTONDOWN:
+            #if event.button == SDL_BUTTON_LEFT:
+                #dirx = x+10
+            #elif event.button == SDL_BUTTON_RIGHT:
+                #driy = y+10
+        #elif event.type == SDL_MOUSEBUTTONUP:
+            #if event.button == SDL_BUTTON_LEFT:
+             #   dirx = x - 10
+            #elif event.button == SDL_BUTTON_RIGHT:
+             #   driy = y - 10
 
 
+
+    #pass
+
+open_canvas(KPU_WIDTH, KPU_HEIGHT)
 
 # fill here
-open_canvas(KPU_WIDTH, KPU_HEIGHT)
 kpu_ground = load_image('KPU_GROUND.png')
 character = load_image('animation_sheet.png')
+hand = load_image('hand_arrow.png')
 
 running = True
 x, y = KPU_WIDTH // 2, KPU_HEIGHT // 2
 frame = 0
-hide_cursor()#r그 머지?
+hide_cursor()
+dirx = 500
+diry = 500
 
 while running:
     clear_canvas()
     kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
-    character.clip_draw(frame * 100, 100 * 1, 100, 100, x, y)
+    character.clip_draw(frame * 100, 100 * 1, 100, 100, dirx, diry)
+    hand.draw_now(x,y)
     update_canvas()
     frame = (frame + 1) % 8
+
 
     delay(0.02)
     handle_events()
 
 close_canvas()
+
+
+
+
 
 
 
