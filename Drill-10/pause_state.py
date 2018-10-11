99ff33f3786f30dbeb04a29d469ea646433f07e3
gjm0408 <gjm940408@naver.com>
2018-10-11 23:36:12 +0900

    2013184007/김지명/Drill10-1/커밋4회
    10-1최종 작동확인
    
    https://youtu.be/TC3geS-Xsdw

--- Drill-10/pause_state.py
+++ Drill-10/pause_state.py
@@ -1,6 +1,41 @@
 import game_framework
 from pico2d import *
+#import main_state
 
 name = "PauseState"
 image = None
 
+def enter():
+    global image
+    image = load_image('pause.png')
+    size = (50,50)
+def exit():
+    global image
+    del(image)
+
+def update():
+    pass
+
+def handle_events():
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            game_framework.quit()
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_p:
+            game_framework.pop_state()
+
+
+
+
+
+
+def draw():
+    clear_canvas()
+    image.draw(800/2, 600/2)
+    update_canvas()
+
+def pause():
+    pass
+
+def resume():
+    pass
